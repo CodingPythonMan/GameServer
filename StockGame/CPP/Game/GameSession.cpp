59de018be86cb7f7ba81fae5b9c20f9f81ed4bba
Kim Ju Young <robolab1902@naver.com>
2025-03-21 17:41:57 +0900

    Godot 이랑 에코가 되긴한데 왜 메세지 하나만 되냐...?

--- StockGame/CPP/Game/GameSession.cpp
+++ StockGame/CPP/Game/GameSession.cpp
@@ -3,6 +3,7 @@
 #include "GameSessionManager.h"
 #include "CoreGlobal.h"
 #include "SendBuffer.h"
+#include "Memory.h"
 
 GameSession::~GameSession()
 {
@@ -25,18 +26,19 @@ void GameSession::OnRecvPacket(BYTE* buffer, int32 len)
 	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
 
 	char recvBuffer[20] = { 0 }; // +1: 널 종료 문자 추가
-	memcpy(recvBuffer, buffer, len); // 수신된 데이터 복사
-	recvBuffer[len+1] = '\0'; // 문자열 종료 문자 추가
+	memcpy(recvBuffer, buffer, len+4); // 수신된 데이터 복사
+	recvBuffer[len+4] = '\0'; // 문자열 종료 문자 추가
 
 	std::cout << "수신된 데이터 (문자열 변환): " << recvBuffer << std::endl;
 
 	// TODO : packetId 대역 체크
-	// 여기가 이제 컨텐츠 들어가는 단인데... 천천히 만들어보자.
 	//ClientPacketHandler::HandlePacket(session, buffer, len);
 
-	SendBufferRef sendBuffer = GSendBufferManager->Open(len - 4);
-	session->Send(sendBuffer);
-	
+	SendBufferRef sendBuffer = GSendBufferManager->Open(len+4); // 동적 버퍼 생성
+	memcpy(sendBuffer->Buffer(), recvBuffer, len+4); // 데이터를 버퍼에 복사
+	sendBuffer->Close(len+4); // 버퍼 크기 설정
+
+	session->Send(sendBuffer); // 클라이언트에게 응답 전송
 }
 
 void GameSession::OnSend(int32 len)
