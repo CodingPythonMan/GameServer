62571e5f2c3089f04e2bbd29da84576c77e4c07b
robo <robolab1902@naver.com>
2025-03-23 22:28:37 +0900

    Protobuf 신버전 넣기가 생각보다 빡세졌다. absl 이라는게 들어갔다.

---
+++ StockGame/CPP/Game/ClientPacketHandler.cpp
@@ -0,0 +1,23 @@
+#include "ClientPacketHandler.h"
+
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+// Á÷Á¢ ÄÁÅÙÃ÷ ÀÛ¾÷ÀÚ
+
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
+{
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+
+	return false;
+}
+
+bool OnCSEchoReq(PacketSessionRef& session, CSEchoReq& req)
+{
+	SCEchoAck ack;
+	ack.set_text(req.text());
+
+	auto sendBuffer = ClientPacketHandler::MakeSendBuffer(ack);
+	session->Send(sendBuffer);
+
+	return true;
+}
---
+++ StockGame/CPP/Game/ClientPacketHandler.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <functional>
+#include "GameSession.h"
+#include "Macro.h"
+#include "CoreGlobal.h"
+#include "SendBuffer.h"
+#include "Protocol.pb.h"
+
+using PacketHandlerFunc = std::function<bool(PacketSessionRef&, BYTE*, int32)>;
+extern PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+enum : uint16
+{
+	CS_EchoReq,
+	SC_EchoAck
+};
+
+// Custom Handlers
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len);
+bool OnCSEchoReq(PacketSessionRef& session, CSEchoReq& pkt);
+
+class ClientPacketHandler
+{
+public:
+	static void Init()
+	{
+		for (int32 i = 0; i < UINT16_MAX; i++)
+		{
+			GPacketHandler[i] = Handle_INVALID;
+		}
+		GPacketHandler[CS_EchoReq] = [](PacketSessionRef& session, BYTE* buffer, int32 len) { return HandlePacket<CSEchoReq>(OnCSEchoReq, session, buffer, len); };
+	}
+
+	static bool HandlePacket(PacketSessionRef& session, BYTE* buffer, int32 len)
+	{
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+		return GPacketHandler[header->id](session, buffer, len);
+	}
+	static SendBufferRef MakeSendBuffer(SCEchoAck& pkt) { return MakeSendBuffer(pkt, SC_EchoAck); }
+
+private:
+	template<typename PacketType, typename ProcessFunc>
+	static bool HandlePacket(ProcessFunc func, PacketSessionRef& session, BYTE* buffer, int32 len)
+	{
+		PacketType pkt;
+		if (pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)) == false)
+			return false;
+
+		return func(session, pkt);
+	}
+
+	template<typename T>
+	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
+	{
+		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
+		const uint16 packetSize = dataSize + sizeof(PacketHeader);
+
+		SendBufferRef sendBuffer = GSendBufferManager->Open(packetSize);
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+		header->size = packetSize;
+		header->id = pktId;
+		ASSERT_CRASH(pkt.SerializeToArray(&header[1], dataSize));
+		sendBuffer->Close(packetSize);
+
+		return sendBuffer;
+	}
+};
