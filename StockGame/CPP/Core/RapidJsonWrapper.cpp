235932800ce0046ceb3904e8f2bf1d3a95ace022
JuYoung Kim <robolab1902@naver.com>
2025-06-04 16:32:47 +0900

    RapidJson 올릴 준비 끝. 해당 Include 파일은 모두 필요하다.

---
+++ StockGame/CPP/Core/RapidJsonWrapper.cpp
@@ -0,0 +1,191 @@
+#include "RapidJsonWrapper.h"
+#include "CoreGlobal.h"
+#include "ConsoleLog.h"
+#include <fstream>
+#include <sstream>
+
+bool RapidJsonWrapper::Parse(const std::string& jsonStr)
+{
+    mDocument.Parse(jsonStr.c_str());
+    if (mDocument.HasParseError())
+    {
+        size_t errOffset = mDocument.GetErrorOffset();
+        auto errCode = mDocument.GetParseError();
+
+        GConsoleLogger->WriteStdOut(Color::RED, 
+            L"[JsonWrapper] JSON 파싱 오류 (offset : %d): %d", errOffset, errCode);
+
+        return false;
+    }
+
+    return true;
+}
+
+bool RapidJsonWrapper::ParseFile(const std::string& filePath)
+{
+    std::string content;
+    if (!_ReadFileToString(filePath, content))
+    {
+        return false;
+    }
+
+    return Parse(content);
+}
+
+bool RapidJsonWrapper::IsObject() const
+{
+    return mDocument.IsObject();
+}
+
+bool RapidJsonWrapper::IsArray() const
+{
+    return mDocument.IsArray();
+}
+
+bool RapidJsonWrapper::HasKey(const std::string& key) const
+{
+    if (false == mDocument.IsObject())
+    {
+        return false;
+    }
+
+    return mDocument.HasMember(key.c_str());
+}
+
+bool RapidJsonWrapper::GetInt(const std::string& key, int& outValue) const
+{
+    if (false == HasKey(key))
+    {
+        return false;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsInt())
+    {
+        return false;
+    }
+    outValue = value.GetInt();
+    return true;
+}
+
+bool RapidJsonWrapper::GetFloat(const std::string& key, float& outValue) const
+{
+    if (false == HasKey(key))
+    {
+        return false;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsNumber())
+    {
+        return false;
+    }
+    outValue = static_cast<float>(value.GetDouble());
+    return true;
+}
+
+bool RapidJsonWrapper::GetString(const std::string& key, std::string& outValue) const
+{
+    if (false == HasKey(key))
+    {
+        return false;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsString())
+    {
+        return false;
+    }
+    outValue = value.GetString();
+    return true;
+}
+
+bool RapidJsonWrapper::GetBool(const std::string& key, bool& outValue) const
+{
+    if (false == HasKey(key))
+    {
+        return false;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsBool())
+    {
+        return false;
+    }
+    outValue = value.GetBool();
+    return true;
+}
+
+int RapidJsonWrapper::GetArraySize(const std::string& key) const
+{
+    if (false == HasKey(key))
+    {
+        return -1;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsArray())
+    {
+        return -1;
+    }
+
+    return static_cast<int>(value.Size());
+}
+
+const rapidjson::Value* RapidJsonWrapper::GetArrayElement(int index) const
+{
+    if (false == mDocument.IsArray())
+    {
+        return nullptr;
+    }
+
+    const rapidjson::Value& value = mDocument;
+    if (index < 0 || index >= static_cast<int>(value.Size()))
+    {
+        return nullptr;
+    }
+    return &value[index];
+}
+
+bool RapidJsonWrapper::GetIntArray(const std::string& key, std::vector<int>& outVec) const
+{
+    if (false == HasKey(key))
+    {
+        return -1;
+    }
+
+    const rapidjson::Value& value = mDocument[key.c_str()];
+    if (false == value.IsArray())
+    {
+        return -1;
+    }
+
+    return static_cast<int>(value.Size());
+}
+
+bool RapidJsonWrapper::GetFloatArray(const std::string& key, std::vector<float>& outVec) const
+{
+    return false;
+}
+
+bool RapidJsonWrapper::GetStringArray(const std::string& key, std::vector<std::string>& outVec) const
+{
+    return false;
+}
+
+bool RapidJsonWrapper::_ReadFileToString(const std::string& filePath, std::string& out) const
+{
+    std::ifstream ifs{ filePath };
+    if (false == ifs.is_open()) 
+    {
+        GConsoleLogger->WriteStdOut(Color::RED,
+            L"[JsonWrapper] 파일 열기 실패: %s" ,filePath);
+
+        return false;
+    }
+    std::ostringstream ss;
+    ss << ifs.rdbuf();
+    out = ss.str();
+
+    return true;
+}
---
+++ StockGame/CPP/Core/RapidJsonWrapper.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "RapidJson/document.h"
+#include "RapidJson/rapidjson.h"
+
+class RapidJsonWrapper
+{
+public:
+    bool Parse(const std::string& jsonStr);
+
+    bool ParseFile(const std::string& filePath);
+
+    bool IsObject() const;
+
+    bool IsArray() const;
+
+    /// 키에 해당하는 값이 존재하는지 검사 (최상위 객체 기준)
+    bool HasKey(const std::string& key) const;
+
+    bool GetInt(const std::string& key, int& outValue) const;
+
+    /// 키에 해당하는 실수(float) 반환
+    bool GetFloat(const std::string& key, float& outValue) const;
+
+    /// 키에 해당하는 문자열(std::string) 반환
+    bool GetString(const std::string& key, std::string& outValue) const;
+
+    /// 키에 해당하는 Boolean 반환
+    bool GetBool(const std::string& key, bool& outValue) const;
+
+    /// 키에 해당하는 배열(Array) 크기 반환
+    int GetArraySize(const std::string& key) const;
+
+    const rapidjson::Value* GetArrayElement(int index) const;
+
+    /// 지정된 키 아래의 배열을 std::vector<int>로 반환
+    bool GetIntArray(const std::string& key, std::vector<int>& outVec) const;
+
+    /// 지정된 키 아래의 배열을 std::vector<float>로 반환
+    bool GetFloatArray(const std::string& key, std::vector<float>& outVec) const;
+
+    /// 지정된 키 아래의 배열을 std::vector<std::string>로 반환
+    bool GetStringArray(const std::string& key, std::vector<std::string>& outVec) const;
+
+private:
+    rapidjson::Document mDocument;
+
+    // JSON 파일을 한 번에 std::string으로 반환
+    bool _ReadFileToString(const std::string& filePath, std::string& out) const;
+};
